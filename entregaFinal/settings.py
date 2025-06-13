fc1c224b265536043d16845a2795d04c2965eb1c
Guillermo Galinski <guille.galinski@gmail.com>
2025-06-12 21:19:51 -0300

    modificaciones entrega

--- entregaFinal/settings.py
+++ entregaFinal/settings.py
@@ -118,9 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static', 
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = '/'
+
